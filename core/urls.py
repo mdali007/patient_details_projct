fb661411e4a06e2fb2b1605e1576959aab7479cf
Muhammed Shameer Ali <92969341+mdali007@users.noreply.github.com>
2025-04-08 21:12:55 +0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('api/patients/', views.get_patients),
     path('api/addpatients/', views.add_patients),
-    path('patient-details/<str:patient_id>/', views.pat_details)
+    path('patient-details/<str:patient_id>/', views.pat_details),
+    path('deletepatient/<str:patient_id>/', views.delete_patient),
 ]
